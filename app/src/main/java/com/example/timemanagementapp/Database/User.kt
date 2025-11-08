6d74a1e94fb2734406ff941d63c1ee224477ba5d
JaswinderKaurSidhu <a55jadwinderkaur@gmail.com>
2025-11-08 14:07:20 +1000

    Changes in Login Page

--- app/src/main/java/com/example/timemanagementapp/Database/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.Timemanagementapp.database
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-@Entity(tableNmae = "users")
-data class Users(
-    @PrimaryKey val email: String,
-    val password : String
-)
